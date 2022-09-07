20e662ca8ec4a9e038cf087275683b62665b1756
madhuryabillava6666 <89290192+madhuryabillava6666@users.noreply.github.com>
2022-09-07 17:57:45 +0530

    Update ApplicationConfiguration.java

--- Q2.SpringPostmanCrudRestful-1/src/main/java/com/app/techworm/main/config/ApplicationConfiguration.java
+++ Q2.SpringPostmanCrudRestful-1/src/main/java/com/app/techworm/main/config/ApplicationConfiguration.java
@@ -3,7 +3,13 @@ package com.app.techworm.main.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+@Data
 @Configuration
+@Getter
+@Setter
 public class ApplicationConfiguration {
 	@Value("${server.port}")
 	private String serverPort;
@@ -26,61 +32,6 @@ public class ApplicationConfiguration {
 	@Value("${spring.jpa.properties.hibernate.dialect}")
 	private String springjpapropertieshibernatedialect;
 
-	public String getServerPort() {
-		return serverPort;
-	}
-
-	public void setServerPort(String serverPort) {
-		this.serverPort = serverPort;
-	}
-
-	public String getSpringdatasourceurl() {
-		return springdatasourceurl;
-	}
-
-	public void setSpringdatasourceurl(String springdatasourceurl) {
-		this.springdatasourceurl = springdatasourceurl;
-	}
-
-	public String getSpringdatasourceusername() {
-		return springdatasourceusername;
-	}
-
-	public void setSpringdatasourceusername(String springdatasourceusername) {
-		this.springdatasourceusername = springdatasourceusername;
-	}
-
-	public String getSpringdatasourcepassword() {
-		return springdatasourcepassword;
-	}
-
-	public void setSpringdatasourcepassword(String springdatasourcepassword) {
-		this.springdatasourcepassword = springdatasourcepassword;
-	}
-
-	public String getSpringjpashowsql() {
-		return springjpashowsql;
-	}
-
-	public void setSpringjpashowsql(String springjpashowsql) {
-		this.springjpashowsql = springjpashowsql;
-	}
-
-	public String getSpringjpahibernateddlauto() {
-		return springjpahibernateddlauto;
-	}
-
-	public void setSpringjpahibernateddlauto(String springjpahibernateddlauto) {
-		this.springjpahibernateddlauto = springjpahibernateddlauto;
-	}
-
-	public String getSpringjpapropertieshibernatedialect() {
-		return springjpapropertieshibernatedialect;
-	}
-
-	public void setSpringjpapropertieshibernatedialect(String springjpapropertieshibernatedialect) {
-		this.springjpapropertieshibernatedialect = springjpapropertieshibernatedialect;
-	}
 	
 	
 }
